47a75f0bd5b5e8d4208e9c686cfa203e8e9f8ddb
Sunil Kumar <sunil@sun1l.com>
2017-04-03 22:44:41 +0530

    Added new options --path and --safemode

--- bin/cli.js
+++ bin/cli.js
@@ -8,44 +8,56 @@ import packageJson from '../../package.json';
  * Fetch the arguments passed
  */
 let argv = require('minimist')(process.argv.slice(2), {
-    boolean: ['overwrite', 'dryrun', 'help', 'version'],
-    alias: {
-        o: 'overwrite',
-        d: 'dryrun',
-        h: 'help',
-        v: 'version'
-    },
-    default: {
-        dryrun: false,
-        overwrite: false,
-        help: false,
-        version: false
-    },
-    unknown: () => {
-    }
+  boolean: ['dryrun', 'safemode', 'overwrite', 'help', 'version'],
+  string: ['path'],
+  alias: {
+    d: 'dryrun',
+    s: 'safemode',
+    o: 'overwrite',
+    p: 'path',
+    h: 'help',
+    v: 'version'
+  },
+  default: {
+    dryrun: false,
+    safemode: true,
+    overwrite: false,
+    path: process.cwd()
+  }
 });
 
-
-let { overwrite, dryrun, help, version } = argv;
+let { dryrun, safemode, overwrite, path, help, version } = argv;
 
 if (version) {
-    console.log(packageJson.version);
-    process.exit();
+  console.log(packageJson.version);
+  process.exit();
 }
 
 if (help || argv._.length < 1) {
-    console.log(
-        chalk.blue(`Usage: ${packageJson.name} <JSON file> [options]`)
-    );
-    process.exit();
+  console.log(
+    chalk.green(
+      `\n` +
+      `Usage: ${packageJson.name} <JSON/YAML file> [options]\n`) +
+      `\n` +
+      `Options:\n` +
+      `-d, --dryrun       only disply the skeleton, don't create any folder/file\n` +
+      `-s, --safemode     create skeleton but don't overwrite existing files (default)\n` +
+      `-o, --overwrite    overwrite any existing file, if already exist\n` +
+      `-p, --path         root path for skeleton, default is current directory\n` +
+      `-h, --help         output usage information\n` +
+      `-v, --version      output the version number\n` +
+      `\n` +
+      `Please visit https://github.com/sun1l/skelenator for examples.\n`
+  );
+  process.exit();
 }
 
-// Set 'overwrite' to false if dryrun is true
-overwrite = dryrun? false : overwrite;
+// Set 'overwrite' to false if dryrun or safemode is true
+overwrite = (dryrun || safemode) ? false : overwrite;
 
 require('../lib/skelenator')(argv._, {
-    overwrite,
-    dryrun,
-    help,
-    version
+  dryrun,
+  safemode,
+  overwrite,
+  path
 });
